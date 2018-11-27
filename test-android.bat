d7f1ad8034098fd1c82b39a5d2a3ada7ce59949f
Hussein Taher <hussein.taher@king.com>
2018-11-27 17:08:06 +0100

    updated test batch script

--- test-android.bat
+++ test-android.bat
@@ -1,4 +1,4 @@
-java -jar bob.jar --debug --archive --platform armv7-android build bundle -bo build/armv7-android
+IF "%1"=="/b" java -jar bob.jar --debug --archive --platform armv7-android build bundle -bo build/armv7-android
 adb uninstall com.defoldexample.videoplayer
 adb install -r build\armv7-android\VideoPlayer\VideoPlayer.apk
 adb shell am start -a android.intent.action.MAIN -n com.defoldexample.videoplayer/com.dynamo.android.DefoldActivity
