261b36bec87e7b61b672bb83b4e9b655626293a7
Hussein Taher <hussein.taher@king.com>
2019-03-18 14:45:49 +0100

    added immersive mode to android

--- videoplayer/src/java/Movie.java
+++ videoplayer/src/java/Movie.java
@@ -74,6 +74,7 @@ class Movie implements
 
 		Logger.log("Movie: new VideoView");
 		videoView = new VideoView((Context)activity);
+		videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
 		final Movie instance = this;
 		videoView.getHolder().addCallback(new SurfaceHolder.Callback(){
